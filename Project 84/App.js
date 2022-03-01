cad21aaf9bf0f3dad96d4f8cb7728c6689e6974f
tejeesha <73623905+tejeesha@users.noreply.github.com>
2022-02-28 19:22:09 -0800

    Add files via upload

---
+++ Project 84/App.js
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import DrawerNavigation from "./navigation/DrawerNavigation";
+
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <DrawerNavigation />
+    </NavigationContainer>
+  );
+}
